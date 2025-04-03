9bcb480ead686c74a6b1a2cc75a41923f288c2eb
EmilOrv <emilols.eo@gmail.com>
2025-04-03 17:36:30 +0200

    add battery level display

--- scooter/Display.py
+++ scooter/Display.py
@@ -4,9 +4,9 @@ sense = SenseHat()
 
 # Define pixel patterns for each status
 STATUS_PATTERNS = {
-    "unlocked": [(1, 3), (2, 2), (3, 1), (4, 2), (5, 3)], 
-    "rented": [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)], 
-    "locked": [(2, 2), (2, 5), (5, 2), (5, 5)], 
+    "Unlocked": [(1, 3), (2, 2), (3, 1), (4, 2), (5, 3)], 
+    "Rented": [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)], 
+    "Locked": [(2, 2), (2, 5), (5, 2), (5, 5)], 
 }
 
 # Default off color (black)
@@ -24,5 +24,31 @@ def display_status(status, color):
 
 def display_text(message, color="white"):
     sense.show_message(message, text_colour=color)
+
+
+def display_battery(percent):
+    """Displays the battery level on the Sense HAT."""
+    pixels = [OFF_COLOR] * 64  # 8x8 grid
+
+    if percent > 50:
+        color = (0, 255, 0)  # Green
+        num_lit_pixels = 3
+    elif 30 <= percent <= 50:
+        color = (255, 255, 0)  # Yellow
+        num_lit_pixels = 2
+    else:
+        color = (255, 0, 0)  # Red
+        num_lit_pixels = 1
+
+    # Define the top-right corner positions
+    top_right_positions = [(7, 0), (7, 1), (7, 2)]  # (x, y) coordinates
+
+    # Light up the appropriate number of pixels
+    for i in range(num_lit_pixels):
+        x, y = top_right_positions[i]
+        pixels[y * 8 + x] = color  # Convert (x, y) to 1D index
+
+    sense.set_pixels(pixels)
+
 
 
